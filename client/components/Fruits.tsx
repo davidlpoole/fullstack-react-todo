8d575a67c1a6fed414831c9646ee4f35a1de2a60
Daphne Simons <daphne.simons@devacademy.co.nz>
2023-11-15 16:44:15 +1300

    update auth0 version - works now

--- client/components/Fruits.tsx
+++ client/components/Fruits.tsx
@@ -6,6 +6,7 @@ import { addFruit, deleteFruit, getFruits, updateFruit } from '../api.ts'
 import SelectedFruitForm from './SelectedFruit.tsx'
 import AddFruitForm from './AddFruit.tsx'
 import { ErrorMessage } from './Styled.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 type State =
   | {
@@ -28,7 +29,7 @@ function Fruits() {
   const [form, setForm] = useState<State>(closedForm)
 
   // TODO: call the useAuth0 hook and destructure getAccessTokenSilently
-
+  const {getAccessTokenSilently} = useAuth0()
   useEffect(() => {
     getFruits()
       .then(setFruits)
@@ -38,7 +39,8 @@ function Fruits() {
   const handleAdd = async (fruit: NewFruit) => {
     try {
       // TODO: pass token as second parameter
-      const fruits = await addFruit(fruit, 'token')
+      const token = await getAccessTokenSilently()
+      const fruits = await addFruit(fruit, token)
 
       setFruits(fruits)
       handleCloseForm()
@@ -53,7 +55,8 @@ function Fruits() {
   const handleUpdate = async (updatedFruit: Fruit) => {
     try {
       // TODO: pass token as second parameter
-      const fruits = await updateFruit(updatedFruit, 'token')
+      const token = await getAccessTokenSilently()
+      const fruits = await updateFruit(updatedFruit, token)
 
       setFruits(fruits)
       handleCloseForm()
@@ -68,7 +71,8 @@ function Fruits() {
   const handleDelete = async (id: number) => {
     try {
       // TODO: pass token as second parameter
-      const fruits = await deleteFruit(id, 'token')
+      const token = await getAccessTokenSilently()
+      const fruits = await deleteFruit(id, token)
 
       setFruits(fruits)
       handleCloseForm()
--- server/auth0.ts
+++ server/auth0.ts
@@ -5,8 +5,8 @@ import type { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
-const audience = 'https://'
+const domain = 'https://pohutukawa-23-daph.au.auth0.com'
+const audience = 'https://fruits/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
